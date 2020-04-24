ebc127554374ce6d55cb694553d0e1cd7f2197c4
M Groesbeck <m.groesbeck@gmail.com>
2020-04-24 08:37:05 -0700

    Endpoints for MVP built and tested.

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,46 @@
 const router = require('express').Router();
+const bcrypt = require("bcryptjs");
+
+const TokenGen = require("../api/token-gen.js");
+const Users = require("./users-model.js");
 
 router.post('/register', (req, res) => {
-  // implement registration
+  let user = req.body;
+  const salter = process.env.HASH_ROUNDS || 14;
+  const pwHash = bcrypt.hashSync(user.password, salter);
+  user.password = pwHash;
+  Users.add(user)
+    .then((saved) => {
+        const token = TokenGen.generateToken(saved);
+      res.status(201).json({ message: "Welcome", token });
+    })
+    .catch((err) => {
+      res.status(500).json({ errorMessage: err.message });
+    });
 });
 
 router.post('/login', (req, res) => {
   // implement login
+  let { username, password } = req.body;
+  Users.findWithPass({ username })
+    .then(([found]) => {
+      if (found) {
+        if (bcrypt.compareSync(password, found.password)) {
+          const token = TokenGen.generateToken(found);
+          res.status(200).json({ message: "Welcome", token });
+        } else {
+          res
+            .status(401)
+            .json({ message: "Authentication failed. You cannot pass!" });
+        }
+      } else {
+        res.status(400).json({ message: "Username and password required. " });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ errorMessage: err.message });
+    });
 });
 
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,23 @@
   before granting access to the next middleware/route handler
 */
 
+const jwt = require("jsonwebtoken");
+
+const secrets = require("../api/secrets.js");
+
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+  const secret = secrets.jwtSecret;
+  if (token) {
+    jwt.verify(token, secret, function (err, decoded) {
+        if (err) {
+          res.status(401).json({ message: "Authentication failed.", err });
+        } else {
+          req.decodedToken = decoded;
+          next();
+        }
+      });
+  } else {
+      res.status(400).json({message: "Please provide token." });
+  }
 };
